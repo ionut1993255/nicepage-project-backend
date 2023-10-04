f154bc59e3ce967c280eaf1fdc6a394dd94cdc5d
ionut <ionut_ilisei@yahoo.com>
2023-10-04 23:56:22 +0300

    base commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Utilizator;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    // Get all the users from the database
+    public function index()
+    {
+        $users = Utilizator::all();
+
+        if ($users->count() > 0) {
+            return response()->json([
+                'status' => 200,
+                'users' => $users
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No records found!'
+            ], 404);
+        }
+    }
+
+    // Store users in the database
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:258',
+            'email' => 'required|email|max:258',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'consent' => 'required|boolean'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        } else {
+            $imageName = Str::random(32) . "." . $request->image->getClientOriginalExtension();
+
+            $user = Utilizator::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'image' => $imageName,
+                'consent' => $request->consent
+            ]);
+
+            if ($user) {
+                Storage::disk('public')->put($imageName, file_get_contents($request->image));
+
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'User created successfully!'
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Something went wrong!'
+                ], 500);
+            }
+        }
+    }
+
+    // Search for a user based on his "id"
+    public function show($id)
+    {
+        $user = Utilizator::find($id);
+
+        if ($user) {
+            return response()->json([
+                'status' => 200,
+                'user' => $user
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No such user found!'
+            ], 404);
+        }
+    }
+
+    // Update a user based on his "id"
+    public function update(Request $request, int $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:258',
+            'email' => 'required|email|max:258',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'consent' => 'required|boolean'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        } else {
+            $user = Utilizator::find($id);
+
+            if ($user) {
+                $user->name = $request->name;
+                $user->email = $request->email;
+                $user->consent = $request->consent;
+
+                if ($request->image) {
+                    $storage = Storage::disk('public');
+
+                    if ($storage->exists($user->image))
+                        $storage->delete($user->image);
+
+                    $imageName = Str::random(32) . "." . $request->image->getClientOriginalExtension();
+                    $user->image = $imageName;
+
+                    $storage->put($imageName, file_get_contents($request->image));
+                }
+
+                $user->save();
+
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'User updated successfully!'
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'No such user found!'
+                ], 404);
+            }
+        }
+    }
+
+    // Delete a user based on his "id"
+    public function destroy($id)
+    {
+        $user = Utilizator::find($id);
+
+        if ($user) {
+            $storage = Storage::disk('public');
+
+            if ($storage->exists($user->image))
+                $storage->delete($user->image);
+
+            $user->delete();
+
+            return response()->json([
+                "status" => 200,
+                'message' => "User deleted successfully!"
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No such user found!'
+            ], 404);
+        }
+    }
+}
